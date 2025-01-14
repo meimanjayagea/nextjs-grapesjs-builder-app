63e460cd2d3474871ea7d6e0b9877b39dafe48e6
jayameiman <workingsdd@gmail.com>
2025-01-15 00:58:33 +0700

    [FIXING APP]-update fixing grapesjs

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 // import StudioEditorBuilder from '@/components/studioeditorbuilder'
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 // import dynamic from 'next/dynamic';
 import {
   Card,
@@ -17,7 +17,6 @@ import StudioEditor, {
 } from '@grapesjs/studio-sdk/react';
 // const StudioEditor = dynamic(() => import('@grapesjs/studio-sdk/react'), {ssr: false});
 import '@grapesjs/studio-sdk/style';
-
 import grapesJsTailwind from 'grapesjs-tailwind';
 
 export default function Home() {
@@ -58,27 +57,42 @@ export default function Home() {
     }
   }
 
+  const getGlobalStyle =()=>{
+    if (editor) {
+      editor.runCommand('studio:layoutToggle', {
+          id: 'gs',
+          layout: 'panelGlobalStyles',
+          header: { label: 'Global Styles' },
+          placer: { type: 'absolute', position: 'right' }
+        })
+    }
+  }
+
+
   return (
     <Card>
       <CardHeader>
         <CardTitle>Builder APP</CardTitle>
         <CardDescription>Create a new project to build a website or homepage application.</CardDescription>
-          <div className="flex h-screen flex-col justify-between p-5 gap-2">
-            <div className="flex gap-5 items-center">
+          <div className="flex h-screen flex-col justify-between pt-4">
+            <div className="flex gap-5 items-center bg-black text-white p-1">
               <div className="font-bold"></div>
-              <button className="border rounded px-2" onClick={getProjetData}>
+              <button className="border rounded px-1" onClick={getProjetData}>
                 Log Project Data
               </button>
-              <button className="border rounded px-2" onClick={getExportData}>
+              <button className="border rounded px-1" onClick={getExportData}>
                 Log HTML/CSS
               </button>
-              <button className="border rounded px-2" onClick={getTailwindPlugin}>
+              <button className="border rounded px-1" onClick={getTailwindPlugin}>
                 Tailwind
+              </button>
+              <button className="border rounded px-1" onClick={getGlobalStyle}>
+                Global Style
               </button>
               <label className="inline-flex items-center cursor-pointer p-2">
                 <input type="checkbox" value='' className="sr-only peer" />
                 <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">{projectType ? 'web' : 'email'}</span>
+                <span className="ms-3 text-sm font-medium text-white dark:text-gray-300">{projectType ? 'web' : 'email'}</span>
               </label>
           </div>
 
@@ -95,8 +109,8 @@ export default function Home() {
               plugins: [
                 grapesJsTailwind,
                 editor => editor.onReady(() => {
-                    editor.runCommand('get-tailwindCss');
-                })
+                    editor.runCommand('studio:get-tailwindCss');
+                }),
               ]
             }}
           />
